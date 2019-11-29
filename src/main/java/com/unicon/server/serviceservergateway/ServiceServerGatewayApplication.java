4c3bf5c03143b62beefaf97816797a4b3fd89736
Antonio Crispin <antonio.crispin@gmail.com>
2019-11-29 16:32:24 -0500

    end

--- src/main/java/com/unicon/server/serviceservergateway/ServiceServerGatewayApplication.java
+++ src/main/java/com/unicon/server/serviceservergateway/ServiceServerGatewayApplication.java
@@ -2,7 +2,11 @@ package com.unicon.server.serviceservergateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class ServiceServerGatewayApplication {
 
