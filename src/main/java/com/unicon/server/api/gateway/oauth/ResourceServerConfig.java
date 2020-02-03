aabb8adc5535e50fc4bc92cbcd08c3c7f14f3944
Antonio Crispin <antonio.crispin@gmail.com>
2020-02-03 00:02:36 -0500

    basic auth

--- src/main/java/com/unicon/server/api/gateway/oauth/ResourceServerConfig.java
+++ src/main/java/com/unicon/server/api/gateway/oauth/ResourceServerConfig.java
@@ -1,59 +1,60 @@
 package com.unicon.server.api.gateway.oauth;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  *
  * @author acrispin
  * @since 12/12/2019
  */
-@RefreshScope
-@Configuration
-@EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+//@RefreshScope
+//@Configuration
+//@EnableResourceServer
+//public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+public class ResourceServerConfig  {
 
-    @Value("${config.security.oauth.jwt.key}")
-    private String jwtKey;
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.tokenStore(tokenStore());
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/rest/security/**").permitAll()
-                .antMatchers(
-                        "/rest/clientes/cliente/**",
-                        "/rest/obras/obra/**",
-                        "/rest/plantas/planta/**",
-                        "/rest/sunat/empresa/**",
-                        "/rest/cotizaciones/cotizacion/**",
-                        "/rest/acuerdos/acuerdo/**",
-                        "/rest/articulos/articulo/**"
-                ).hasAnyRole("ADMIN", "USER")
-                .anyRequest().authenticated();
-    }
-
-    @Bean
-    public JwtTokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey(jwtKey);
-        return tokenConverter;
-    }
+//    @Value("${config.security.oauth.jwt.key}")
+//    private String jwtKey;
+//
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+//        resources.tokenStore(tokenStore());
+//    }
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/rest/security/**").permitAll()
+//                .antMatchers(
+//                        "/rest/clientes/cliente/**",
+//                        "/rest/obras/obra/**",
+//                        "/rest/plantas/planta/**",
+//                        "/rest/sunat/empresa/**",
+//                        "/rest/cotizaciones/cotizacion/**",
+//                        "/rest/acuerdos/acuerdo/**",
+//                        "/rest/articulos/articulo/**"
+//                ).hasAnyRole("ADMIN", "USER")
+//                .anyRequest().authenticated();
+//    }
+//
+//    @Bean
+//    public JwtTokenStore tokenStore() {
+//        return new JwtTokenStore(accessTokenConverter());
+//    }
+//
+//    @Bean
+//    public JwtAccessTokenConverter accessTokenConverter() {
+//        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+//        tokenConverter.setSigningKey(jwtKey);
+//        return tokenConverter;
+//    }
 
 }
